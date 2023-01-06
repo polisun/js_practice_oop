7053cb9f3ec7d3f057a56495b827b9933e2bcbe9
polisun <p.salamatina@yandex.ru>
2023-01-06 17:16:55 +0400

    practice done

--- src/es6.js
+++ src/es6.js
@@ -5,26 +5,79 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName(fio) {}
+function fioToName(fio = 'Попов Илья Петрович') {
+    const name = fio.split(' ');
+    return name[1] + ' ' + name[0];
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+function filterUnique(array) {
+    let newSet = new Set(array);
+    return Array.from(newSet);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference(array) {}
+function calculateSalaryDifference(array) {
+    try {
+        const max = array.reduce(function (acc, item) {
+            if (acc < item) acc = item;
+            return acc;
+        });
+        const min = array.reduce(function (acc, item) {
+            if (acc > item) acc = item;
+            return acc;
+        });
+        return max / min;
+    } catch (error) {
+        console.log('size === 0');
+    }
+}
 
 // Реализуйте класс "словарь слов" (как толковый словарь)
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // Словарь - (string, string), и все это не null и не undefined
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor() {
+        this.map = new Map();
+    }
+    get(def) {
+        if (typeof def === 'string') return this.map.get(def);
+        else return false;
+    }
+    set(def, desc) {
+        if (typeof def === 'string' && typeof desc === 'string') {
+            this.map.set(def, desc);
+        } else return false;
+    }
+    showSelectedWord(def) {
+        if (typeof def === 'string') {
+            if (this.map.has(def)) console.log(def + ' ' + this.get(def));
+            else return false;
+        } else return false; //неверный тип
+    }
+    remove(def) {
+        if (typeof def === 'string') {
+            if (this.map.has(def)) {
+                this.map.delete(def);
+                return true;
+            } else return false; //нет в словаре
+        } else return false; //неверный тип
+    }
+
+    showAllWords() {
+        for (let key of this.map.keys()) {
+            console.log(key + ' ' + this.get(key));
+        }
+    }
+}
 
 module.exports = {
     fioToName,
